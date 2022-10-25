09e0abccad8e62f008f96ccf657c4566053a4fb3
geekyAdarsh <adarshjain9628@gmail.com>
2022-10-26 02:28:02 +0530

    Add files via upload

---
+++ mli-nuxt/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-navbar>
+    <template #brand>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img src="/img.png">
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component({
+  name: 'Navbar'
+})
+export default class Navbar extends Vue {
+
+}
+</script>
---
+++ mli-nuxt/nuxt.config.js
@@ -0,0 +1,53 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'MLI',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''},
+      {name: 'format-detection', content: 'telephone=no'}
+    ],
+    link: [
+      {rel: 'icon', type: 'image/x-icon', href: '/img.png'}
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    '~/plugins/global.ts'
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/axios',
+    'nuxt-buefy'
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  serverMiddleware: [
+    '~/api/index'
+  ]
+}
